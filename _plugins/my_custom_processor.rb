567edce3809656beb1da0bd1954a834270062695
ruukr8080 <ruukr8080@gmail.com>
2024-06-28 19:17:23 +0900

    just commit

--- _plugins/my_custom_processor.rb
+++ _plugins/my_custom_processor.rb
@@ -1,21 +1,25 @@
-# # _plugins/my_custom_processor.rb
-# module Jekyll
-#     module Converters
-#       module Markdown
-#         class MyCustomProcessor
-#           def initialize(config)
-#             require 'funky_markdown'
-#             @config = config
-#           rescue LoadError
-#             STDERR.puts 'You are missing a library required for Markdown. Please run:'
-#             STDERR.puts '  $ [sudo] gem install funky_markdown'
-#             raise FatalException.new("Missing dependency: funky_markdown")
-#           end
+# _plugins/my_custom_processor.rb
+module Jekyll
+    module Converters
+      module Markdown
+        class MyCustomProcessor
+          def initialize(config)
+            require Kramdown
+            @config = config
+          rescue LoadError
+            STDERR.puts 'You are missing a library required for Markdown. Please run:'
+            STDERR.puts '  $ [sudo] gem install funky_markdown'
+            raise FatalException.new("Missing dependency: funky_markdown")
+        rescue => e
+            STDERR.puts "An error occurred while initializing MyCustomProcessor: #{e.message}"
+            raise FatalException.new("Error initializing MyCustomProcessor")
+        end
+          
   
-#           def convert(content)
-#             ::FunkyMarkdown.new(content).convert
-#           end
-#         end
-#       end
-#     end
-#   end
+          def convert(content)
+            ::Kramdown.new(content).convert
+          end
+        end
+      end
+    end
+  end
