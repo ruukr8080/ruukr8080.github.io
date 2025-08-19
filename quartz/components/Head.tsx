78c1bc99379f289eaab450e564a8a9eb49e4cdba
ruukr8080 <131014787+ruukr8080@users.noreply.github.com>
2025-08-19 18:10:44 +0900

    Update Head.tsx
    37lines :

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -34,7 +34,8 @@ export default (() => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
+{/*         {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />} */}
+        {cfg.baseUrl && <meta property="og:image" content= 'https://github.com/ruukr8080/ruukr8080/blob/main/resources/blackWargraymonjpg.jpg?raw=true' />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
         <link rel="icon" href={iconPath} />
